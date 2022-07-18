0461d7c005523e38c86651bb925b3811b2b0e8a6
revol-xut <revol-xut@protonmail.com>
2022-07-18 21:46:40 +0200

    wrong table name

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -541,7 +541,7 @@ impl DataBaseConnection {
     }
 
     pub  fn update_region(&mut self, region: &Region) -> bool {
-        match self.postgres.execute("UPDATE region SET name=$1, transport_company=$2, frequency=$3, protocol=$4 WHERE id=$5",
+        match self.postgres.execute("UPDATE regions SET name=$1, transport_company=$2, frequency=$3, protocol=$4 WHERE id=$5",
                               &[&region.name, &region.transport_company, &(region.frequency as i64), &region.protocol, &(region.id as i64)]) {
             Err(e) => {
                 println!("Error updating region: {:?}", e);
